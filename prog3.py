e627d2fea1f0f73e94ff58c07c1c0ecaca8a6aef
Prarthana Gowda <p6361874024@gmail.com>
2025-08-02 10:11:43 +0530

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,8 @@
+"""my_tuple=("appple","bannana","orange")
+for item in my_tuple:
+    print(item)"""
+"""list1=[1,2,3]
+list2=[4,5,6]
+combine_list=list1+list2
+print("combined list",combine_list)
+"""
