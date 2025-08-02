ad951009b9ea3ce746fc062a1bcd96bb20ecaeea
Prarthana Gowda <p6361874024@gmail.com>
2025-08-02 09:39:55 +0530

    Add files via upload

---
+++ without.dsp.py
@@ -0,0 +1,13 @@
+name1=input("enter a name")
+marks1=int(input("enter a marks"))
+nams2=input("enter amarks")
+marks2=int(input("enter a marks"))
+nams3=input("enter amarks")
+marks3=int(input("enter a marks"))
+if marks1>marks2 and marks1>marks3:
+    print("topper is",name1)
+elif marks2>marks1 and marks2>3:
+    print("topper is",nams2) 
+else:
+    print("topper is",nams3)       
+    
